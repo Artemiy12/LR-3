c5235d525fb7ed41347ca724bf0388191228b271
Artemiy12 <86324086+Artemiy12@users.noreply.github.com>
2021-11-13 13:58:11 +0300

    Add files via upload

---
+++ Test for LR3/Test for LR3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test_for_LR3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
